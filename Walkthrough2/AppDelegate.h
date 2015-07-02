f36f503efe2e114df6c8961078cccb3529bdfe9b
Brexton Pham <brextonpham@stanford.edu>
2015-07-02 18:40:35 -0400

    Initial Commit

---
+++ Walkthrough2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Walkthrough2
+//
+//  Created by Brexton Pham on 7/2/15.
+//  Copyright (c) 2015 Brexton Pham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Walkthrough2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Walkthrough2
+//
+//  Created by Brexton Pham on 7/2/15.
+//  Copyright (c) 2015 Brexton Pham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
