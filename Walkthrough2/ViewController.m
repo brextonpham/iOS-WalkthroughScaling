014e946c2e1708aba0ebd9cb157d9cfdde421851
Brexton Pham <brextonpham@stanford.edu>
2015-07-07 15:30:33 -0400

    Uploading WalkthroughScaling

--- Walkthrough2/ViewController.m
+++ Walkthrough2/ViewController.m
@@ -8,20 +8,134 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIScrollViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+
+@property (weak, nonatomic) IBOutlet UILabel *label;
+@property (weak, nonatomic) IBOutlet UIImageView *image;
+
+@property (nonatomic, strong) NSArray *pageImages;
+@property (nonatomic, strong) NSMutableArray *pageViews;
+
+- (void)loadVisiblePages;
+- (void)loadPage:(NSInteger)page;
+- (void)purgePage:(NSInteger)page;
 
 @end
 
+CGFloat offsetX;
+CGFloat percentageOffset;
+
 @implementation ViewController
+
+@synthesize scrollView = _scrollView;
+@synthesize pageImages = _pageImages;
+@synthesize pageViews = _pageViews;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // set up page images
+    self.pageImages = [NSArray arrayWithObjects:
+                       [UIImage imageNamed:@"whitebackground.png"],
+                       [UIImage imageNamed:@"whitebackground.png"],
+                       nil];
+    
+    NSInteger pageCount = self.pageImages.count;
+    
+    // set up array that holds UIImageView instances
+    self.pageViews = [[NSMutableArray alloc] init];
+    for (NSInteger i = 0; i < pageCount; ++i) {
+        [self.pageViews addObject:[NSNull null]];
+    }
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    // telling scroll view its content size for horizontal scroll view
+    CGSize pagesScrollViewSize = self.scrollView.frame.size;
+    self.scrollView.contentSize = CGSizeMake(pagesScrollViewSize.width * self.pageImages.count, pagesScrollViewSize.height);
+    
+    // have pages shown initially
+    [self loadVisiblePages];
 }
+
+- (void)loadPage:(NSInteger)page {
+    if (page < 0 || page >= self.pageImages.count) {
+        // If it's outside the range of what you have to display, then do nothing
+        return;
+    }
+    
+    // if we haven't loaded the view, then the object in pageViews array will be an NSNull
+    UIView *pageView = [self.pageViews objectAtIndex:page];
+    if ((NSNull*)pageView == [NSNull null]) {
+        // create a page
+        CGRect frame = self.scrollView.bounds;
+        frame.origin.x = frame.size.width * page;
+        frame.origin.y = 0.0f;
+        
+        // creates new image view and adds it to scroll view
+        UIImageView *newPageView = [[UIImageView alloc] initWithImage:[self.pageImages objectAtIndex:page]];
+        //newPageView.contentMode = UIViewContentModeScaleAspectFit;
+        newPageView.frame = frame;
+        [self.scrollView addSubview:newPageView];
+        // replace NSNull in pageViews array with view we just created
+        [self.pageViews replaceObjectAtIndex:page withObject:newPageView];
+    }
+}
+
+// method purges a page that was previously created: it first checks that the object in the pageViews array for this
+// page is not an NSNull. If it's not, it removes the view from the scroll view and updates the pageViews array with an
+// NSNull again to indicate that this page is no longer there
+- (void)purgePage:(NSInteger)page {
+    if (page < 0 || page >= self.pageImages.count) {
+        // If it's outside the range of what you have to display, then do nothing
+        return;
+    }
+    
+    // Remove a page from the scroll view and reset the container array
+    UIView *pageView = [self.pageViews objectAtIndex:page];
+    if ((NSNull*)pageView != [NSNull null]) {
+        [pageView removeFromSuperview];
+        [self.pageViews replaceObjectAtIndex:page withObject:[NSNull null]];
+    }
+}
+
+- (void)loadVisiblePages {
+    // First, determine which page is currently visible
+    CGFloat pageWidth = self.scrollView.frame.size.width;
+    NSInteger page = (NSInteger)floor((self.scrollView.contentOffset.x * 2.0f + pageWidth) / (pageWidth * 2.0f));
+    
+    // Work out which pages you want to load
+    NSInteger firstPage = page - 1;
+    NSInteger lastPage = page + 1;
+    
+    // Purge anything before the first page
+    for (NSInteger i=0; i<firstPage; i++) {
+        [self purgePage:i];
+    }
+    
+    // Load pages in our range
+    for (NSInteger i=firstPage; i<=lastPage; i++) {
+        [self loadPage:i];
+    }
+    
+    // Purge anything after the last page
+    for (NSInteger i=lastPage+1; i<self.pageImages.count; i++) {
+        [self purgePage:i];
+    }
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    // Load the pages that are now on screen
+    [self loadVisiblePages];
+    offsetX = scrollView.contentOffset.x;
+    percentageOffset = offsetX / (scrollView.contentSize.width - scrollView.bounds.size.width);
+    self.image.layer.transform = CATransform3DMakeScale(((offsetX * 0.01) + 1.0), ((offsetX * 0.01) + 1.0), 1.0);
+    self.label.text = [NSString stringWithFormat: @"%.2f", percentageOffset];
+}
+
 
 @end
